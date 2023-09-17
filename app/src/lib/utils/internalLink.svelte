d6cc51f62bab3cbd65b2bc49764751e271bcfeaf
vhollo <vhollo@protonmail.com>
2023-09-17 22:39:43 +0200

    Initial… commit

---
+++ app/src/lib/utils/internalLink.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type {MarkComponentProps} from '@portabletext/svelte'
+
+  // Property custom marks receive from @portabletext/svelte when redered
+  export let portableText: MarkComponentProps<{
+    slug?: { _type: string, current: string }
+  }>
+
+  // Remember to make your variables reactive so that they can reflect prop changes
+  // See: https://svelte.dev/docs#3_$_marks_a_statement_as_reactive
+  $: ({value} = portableText)
+  //$: newWindow = value.newWindow || false
+</script>
+
+{#if value.slug}
+  <a href={value.slug.current}>
+    <slot />
+  </a>
+{:else}
+  <slot />
+{/if}
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-	import Card from '../components/Card.svelte';
-	import Welcome from '../components/Welcome.svelte';
+	//import Card from '../components/Card.svelte';
+	//import Welcome from '../components/Welcome.svelte';
+	import { PortableText } from '@portabletext/svelte';
+	import { formatDate } from '$lib/utils';
+	import internalLink from '$lib/utils/internalLink.svelte';
+	import { urlFor } from '$lib/utils/image';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+  //console.log(data.body)
+  const formatBody = {
+    marks: {
+      internalLink: internalLink,
+      /*link: ({value, children}) => {
+        // Read https://css-tricks.com/use-target_blank/
+        const { blank, href } = value
+        return blank ?
+          `<a href=${href} target="_blank" rel="noopener">{children}</a>
+          ` : `<a href=${href}>{children}</a>
+          `
+      }*/
+    }
+  }
 </script>
 
+<!--
 <section>
 	{#if data.posts.length}
 		{#each data.posts as post}
@@ -15,3 +34,42 @@
 		<Welcome />
 	{/if}
 </section>
+-->
+
+<article class="post prose gap-x-0 gap-y-0">
+
+  {#if !!data.mainImage}
+    <figure class="mainimg">
+      <img
+        class="post__contains"
+        src={urlFor(data.mainImage).url()}
+        alt="Cover image for {data.title}"
+      />
+    </figure>
+  {:else}
+    <p class="post__cover--none" />
+  {/if}
+  <header>
+    <h2 class="post__title">{data.title}</h2>
+    {#if data.excerpt}<p class="post__excerpt">{data.excerpt}</p>{/if}
+    {#if data.cta}
+    <aside>
+      <a href={`${data.cta.link}`}>[&thinsp;<button>{data.cta.text}]</button></a>
+    </aside>
+    {/if}
+  </header>
+  <date class="post__date text-sm">Frissítve: 
+    {formatDate(data._createdAt)}
+  </date>
+  <PortableText value={data.body} components={formatBody}/>
+</article>
+
+<style>
+  article :global(p) {
+    margin-block: 0;
+  }
+
+  /*article {
+    @apply mx-4;
+  }*/
+</style>
