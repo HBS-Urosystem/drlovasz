afd3324ca6f155a1290a1036c8537f8bc8cf0065
vhollo <vhollo@protonmail.com>
2024-03-24 22:36:37 +0100

    made with not

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -88,12 +88,10 @@
         xmlns="http://www.w3.org/2000/svg"
         ><path
           d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-          fill="currentColor"
-          stroke="currentColor"
+          fill="#52b2d6"
+          stroke="#52b2d6"
           stroke-width="1.2"
-        /></svg
-      >&nbsp;(not&nbsp;
-      <svg 
+        /></svg>&nbsp;not&nbsp;<svg 
       width="1em"
       height="1em"
       xmlns="http://www.w3.org/2000/svg"
@@ -170,7 +168,7 @@
           C215.11,306.774,229.932,314.259,246.475,314.259z M246.475,226.259c18.748,0,34,15.252,34,34c0,18.747-15.252,34-34,34
           c-18.748,0-34-15.253-34-34C212.475,241.511,227.727,226.259,246.475,226.259z"/>
       </g>
-    </svg>s)&nbsp;at&nbsp;<a href="https://www.urosystem.com/hu">UroSystem</a>
+    </svg>&nbsp;at&nbsp;<a href="https://www.urosystem.com/hu">UroSystem</a>
     </p>
   </div>
 </footer>
